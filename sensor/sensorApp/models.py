570059f33d55368c429b5cb92971173f8493720b
Eduardo Lima <eduardolimrib@gmail.com>
2019-05-18 19:33:57 -0300

    Updated primary keys on models

--- sensor/sensorApp/models.py
+++ sensor/sensorApp/models.py
@@ -17,7 +17,10 @@ class SensorInformation(models.Model):
                                          decimal_places=2)
     minTemperature = models.DecimalField(max_digits=None, 
                                          decimal_places=2)
-    sensorId = models.IntegerField(blank=False)
+    sensorId = models.IntegerField(blank=False,
+                                   primary_key=True,
+                                   unique=True,
+                                   default=1)
 
 
 class Measurement(models.Model):
@@ -28,18 +31,42 @@ class Measurement(models.Model):
                                      auto_now_add=False)       
     initialTime = models.DateTimeField(auto_now=False, 
                                        auto_now_add=False)
-    measurementId = models.IntegerField(blank=False)
+    measurementId = models.IntegerField(blank=False,
+                                        primary_key=True,
+                                        unique=True,
+                                        default=1)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('blog_post_detail', (),
+                {
+                   'id': self.measurementId,
+                })
+
+    def save(self, *args, **kwargs):
+        super(Post, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['created_on']
+        def __unicode__(self):
+            return self.title
+
 
 
 class System(models.Model):
-    systemId = models.IntegerField(blank=False)    
+    systemId = models.IntegerField(blank=False,
+                                   primary_key=True,
+                                   unique=True,
+                                   default=1)   
     systemType = models.CharField(max_length=1,
-                                  choices=VOTE_CHOICES,
+                                  choices=SYSTEM_CHOICES,
                                   blank=True)            
 
 class Product(models.Model):
     productName = models.CharField(max_length=100)
     productQuantity = models.IntegerField(blank=False)
-    productId = models.IntegerField(blank=False)
+    productId = models.IntegerField(blank=False,
+                                    primary_key=True,
+                                    unique=True,
+                                    default=1)
 
-    
