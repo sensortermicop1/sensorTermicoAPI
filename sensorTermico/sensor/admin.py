a93fcc350a1a1d75a62f8d724dcd107598219ec3
Eduardo Lima <eduardolimrib@gmail.com>
2019-06-11 18:59:21 -0300

    Modified admin.py settings

--- sensorTermico/sensor/admin.py
+++ sensorTermico/sensor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Measurement, SensorInformation, System, Product 
 # Register your models here.
+
+admin.site.register(Measurement)
+admin.site.register(SensorInformation)
+admin.site.register(System)
+admin.site.register(Product)
