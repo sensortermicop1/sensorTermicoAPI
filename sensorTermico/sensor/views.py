d59e287ab22286f80f1ad21632b024ddb334947a
Eduardo Lima <eduardolimrib@gmail.com>
2019-06-11 19:30:03 -0300

    Update url behaviour

--- sensorTermico/sensor/views.py
+++ sensorTermico/sensor/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .views import Measurement
+from .models import Measurement
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    latest_measurement_list =  Measurement.objects.order_by('-finalTime')[:5]
+    output = ', '.join([m.temperature for m in latest_measurement_list])
+    return HttpResponse(output)
+
+def detail(request, measurement_id):
+    return HttpResponse("You're looking at measurement %s." % measurement_id)
