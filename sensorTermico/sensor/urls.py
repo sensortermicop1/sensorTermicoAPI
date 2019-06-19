e09ffff67339420150cac5a1a855cf5e2e65de53
Eduardo Lima <eduardolimrib@gmail.com>
2019-06-19 11:20:38 -0300

    Updated urls

--- sensorTermico/sensor/urls.py
+++ sensorTermico/sensor/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'sensor'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:measurement_id>/', views.detail, name='detail'),
+    path('detail/<int:sensor_id>', views.sensor_detail, name='sensor_detail'),
 ]
