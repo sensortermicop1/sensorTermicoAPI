7c1000cbfcda66e279a00f55a7c35a3517f86d99
Eduardo Lima <eduardolimrib@gmail.com>
2019-06-21 16:00:08 -0300

    Now getting temperature

--- sensorTermico/sensor/urls.py
+++ sensorTermico/sensor/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:measurement_id>/', views.detail, name='detail'),
     path('detail/<int:sensor_id>', views.sensor_detail, name='sensor_detail'),
+    path('measurement/', views.get_data, name='measurement'),
 ]
--- sensorTermico/sensor/views.py
+++ sensorTermico/sensor/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Measurement, SensorInformation
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
 
@@ -15,3 +17,9 @@ def detail(request, measurement_id):
 def sensor_detail(request, sensor_id):
     sensor = get_object_or_404(SensorInformation, pk = sensor_id)
     return render(request, "sensor/sensor.html", {'sensor' : sensor})
+
+def get_data(request):
+    page = requests.get('http://192.168.0.13/')
+    soup = BeautifulSoup(page.text , 'html.parser')
+    temperature = soup.get_text()
+    return render(request, 'sensor/measurement.html', {'temperature' : temperature })
